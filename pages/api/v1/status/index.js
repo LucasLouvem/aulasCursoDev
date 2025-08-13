90c10007ad97256079a0540ce92597fe01e104f5
lucas Louvem <lucaslouvem2@gmail.com>
2025-08-13 16:15:11 -0300

    add scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
-import database from "/infra/database.js";
+import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ texto: "Alunos do curso.dev caçando" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections;
+
+  const dbOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const dbOpenedConnectionValue = dbOpenedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenedConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
